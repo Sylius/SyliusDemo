35230da21088bf56c19b4c680c6c2093e710d66e
Mateusz Zalewski <zaleslaw@gmail.com>
2015-06-25 15:00:43 +0200

    Prevent disable SyliusDemo admin

---
+++ src/AppBundle/EventListener/AdminUpdateListener.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\EventListener;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Model\UserInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\File\Exception\UnexpectedTypeException;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+/**
+ * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
+ */
+class AdminUpdateListener
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * Constructor.
+     *
+     * @param Session $session
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param GenericEvent $event
+     */
+    public function preUpdate(GenericEvent $event)
+    {
+        if (!($subject = $event->getSubject()) instanceof CustomerInterface) {
+            throw new UnexpectedTypeException($subject, 'Sylius\Component\Core\Model\CustomerInterface');
+        }
+
+        if ($this->assertCustomerIsDemoAdmin($subject)) {
+            $this->session->getFlashBag()->add("error", "You cannot disable Sylius Demo administrator!");
+
+            $event->stopPropagation();
+        }
+    }
+
+    /**
+     * @param CustomerInterface $customer
+     *
+     * @return boolean
+     */
+    private function assertCustomerIsDemoAdmin(CustomerInterface $customer)
+    {
+        return ('sylius@example.com' === $customer->getEmail() && $customer->getUser()->hasRole('ROLE_SYLIUS_ADMIN') && !$customer->getUser()->isEnabled());
+    }
+}
--- src/AppBundle/spec/EventListener/AdminUpdateListenerSpec.php
+++ src/AppBundle/spec/EventListener/AdminUpdateListenerSpec.php
@@ -3,14 +3,15 @@
 namespace spec\AppBundle\EventListener;
 
 use PhpSpec\ObjectBehavior;
-use Sylius\Component\User\Model\UserInterface;
+use Prophecy\Argument;
 use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\User\Model\UserInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\File\Exception\UnexpectedTypeException;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class AdminDeleteListenerSpec extends ObjectBehavior
+class AdminUpdateListenerSpec extends ObjectBehavior
 {
     function let(Session $session)
     {
@@ -19,10 +20,10 @@ class AdminDeleteListenerSpec extends ObjectBehavior
 
     function it_is_initializable()
     {
-        $this->shouldHaveType('AppBundle\EventListener\AdminDeleteListener');
+        $this->shouldHaveType('AppBundle\EventListener\AdminUpdateListener');
     }
 
-    function it_is_triggered_pre_delete(
+    function it_is_triggered_pre_update(
         GenericEvent $event,
         CustomerInterface $customer,
         UserInterface $user,
@@ -33,21 +34,22 @@ class AdminDeleteListenerSpec extends ObjectBehavior
 
         $customer->getUser()->willReturn($user);
         $user->hasRole('ROLE_SYLIUS_ADMIN')->willReturn(true);
+        $user->isEnabled()->willReturn(false);
 
         $customer->getEmail()->willReturn('sylius@example.com');
 
         $session->getFlashBag()->willReturn($flashBag);
-        $flashBag->add("error", "You cannot delete administrator of Sylius Demo!")->shouldBeCalled();
+        $flashBag->add("error", "You cannot disable Sylius Demo administrator!")->shouldBeCalled();
 
         $event->stopPropagation()->shouldBeCalled();
 
-        $this->preDelete($event);
+        $this->preUpdate($event);
     }
 
     function it_throws_exception_if_event_subject_is_not_customer(GenericEvent $event)
     {
         $event->getSubject()->willReturn('badObject');
 
-        $this->shouldThrow(new UnexpectedTypeException('badObject', 'Sylius\Component\Core\Model\CustomerInterface'))->during('preDelete', array($event));
+        $this->shouldThrow(new UnexpectedTypeException('badObject', 'Sylius\Component\Core\Model\CustomerInterface'))->during('preUpdate', array($event));
     }
 }
