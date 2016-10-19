a8d98783dc95d88e405d8cd876c5e935010e74c4
Paweł Jędrzejewski <pawel@sylius.org>
2016-10-19 18:02:57 +0200

    Configure Platform.sh

---
+++ app/config/parameters_platform.php
@@ -0,0 +1,27 @@
+<?php
+
+$relationships = getenv("PLATFORM_RELATIONSHIPS");
+
+if (!$relationships) {
+    return;
+}
+
+$relationships = json_decode(base64_decode($relationships), true);
+
+foreach ($relationships['database'] as $endpoint) {
+    if (empty($endpoint['query']['is_master'])) {
+      continue;
+    }
+
+    $container->setParameter('database_driver', 'pdo_' . $endpoint['scheme']);
+    $container->setParameter('database_host', $endpoint['host']);
+    $container->setParameter('database_port', $endpoint['port']);
+    $container->setParameter('database_name', $endpoint['path']);
+    $container->setParameter('database_user', $endpoint['username']);
+    $container->setParameter('database_password', $endpoint['password']);
+    $container->setParameter('database_path', '');
+}
+
+$container->setParameter('sylius.cache', array('type' => 'array'));
+
+ini_set('session.save_path', '/tmp/sessions');
