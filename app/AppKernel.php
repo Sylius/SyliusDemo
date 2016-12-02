807c637f9fc13b7e70a657676eef18aa60725a6b
Paweł Jędrzejewski <pawel@sylius.org>
2016-12-02 08:22:31 +0100

    Upgrade to BETA

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,9 +1,19 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Sylius\Bundle\CoreBundle\Application\Kernel;
 
 /**
  * @author Paweł Jędrzejewski <pawel@sylius.org>
+ * @author Gonzalo Vilaseca <gvilaseca@reiss.co.uk>
  */
 class AppKernel extends Kernel
 {
@@ -13,16 +23,17 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
-            new Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
-            new Sylius\Bundle\ApiBundle\SyliusApiBundle(),
-            new Sylius\Bundle\ShopBundle\SyliusShopBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
-            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
-            new AppBundle\AppBundle(),
+            new \Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
+            new \Sylius\Bundle\ShopBundle\SyliusShopBundle(),
+
+            new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
+            new \Sylius\Bundle\ApiBundle\SyliusApiBundle(),
+
+            new \AppBundle\AppBundle(),
         ];
 
         if ('prod' === $this->getEnvironment()) {
-            $bundles[] = new Snc\RedisBundle\SncRedisBundle();
+            $bundles[] = new \Snc\RedisBundle\SncRedisBundle();
         }
 
         return array_merge(parent::registerBundles(), $bundles);
