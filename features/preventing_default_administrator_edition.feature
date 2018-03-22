5bc8255869e98bfef3fd9a2d9927901e539ddabc
Mateusz Zalewski <zaleslaw@gmail.com>
2018-03-22 18:40:02 +0100

    Describe preventing default administrator edition

---
+++ features/preventing_default_administrator_edition.feature
@@ -0,0 +1,19 @@
+@managing_administrators
+Feature: Preventing default administrator edition
+    In order to prevent breaking Sylius Demo application
+    As an Administrator
+    I want to not be able to edit administrator with email "sylius@example.com"
+
+    Background:
+        Given there is also an administrator "admin@example.com"
+        And there is an administrator "sylius@example.com"
+        And I am logged in as "admin@example.com" administrator
+
+    @ui
+    Scenario: Being prevented from edition default administrator
+        When I want to edit administrator "sylius@example.com"
+        And I change its name to "Jon Snow"
+        And I change its email to "jon@example.com"
+        And I save my changes
+        Then I should be notified that I cannot edit Administrator of Sylius Demo
+        And I should see the administrator "sylius@example.com" in the list
--- tests/Behat/Context/Ui/Admin/ManagingAdministratorsContext.php
+++ tests/Behat/Context/Ui/Admin/ManagingAdministratorsContext.php
@@ -6,16 +6,44 @@ namespace Tests\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Context\Context;
 use Sylius\Behat\NotificationType;
+use Sylius\Behat\Page\Admin\Administrator\UpdatePageInterface;
 use Sylius\Behat\Service\NotificationCheckerInterface;
+use Sylius\Component\Core\Model\AdminUser;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Webmozart\Assert\Assert;
 
 final class ManagingAdministratorsContext implements Context
 {
     /** @var NotificationCheckerInterface */
     private $notificationChecker;
 
-    public function __construct(NotificationCheckerInterface $notificationChecker)
-    {
+    /** @var RepositoryInterface */
+    private $adminUserRepository;
+
+    /** @var UpdatePageInterface */
+    private $adminUserUpdatePage;
+
+    public function __construct(
+        NotificationCheckerInterface $notificationChecker,
+        RepositoryInterface $adminUserRepository,
+        UpdatePageInterface $adminUserUpdatePage
+    ) {
         $this->notificationChecker = $notificationChecker;
+        $this->adminUserRepository = $adminUserRepository;
+        $this->adminUserUpdatePage = $adminUserUpdatePage;
+    }
+
+    /**
+     * @When I want to edit administrator :email
+     */
+    public function iWantToEditAdministrator(string $email): void
+    {
+        /** @var AdminUser $admin */
+        $admin = $this->adminUserRepository->findOneBy(['email' => $email]);
+
+        Assert::notNull($admin);
+
+        $this->adminUserUpdatePage->open(['id' => $admin->getId()]);
     }
 
     /**
@@ -25,6 +53,17 @@ final class ManagingAdministratorsContext implements Context
     {
         $this->notificationChecker->checkNotification(
             'You cannot delete administrator of Sylius Demo!',
+            NotificationType::failure()
+        );
+    }
+
+    /**
+     * @Then I should be notified that I cannot edit Administrator of Sylius Demo
+     */
+    public function iShouldBeNotifierThatICannotEditAdministratorOfSyliusDemo(): void
+    {
+        $this->notificationChecker->checkNotification(
+            'You cannot edit administrator of Sylius Demo!',
             NotificationType::failure()
         );
     }
