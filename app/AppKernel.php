d6a08b04e928719551283230603fb8720e3685d5
Łukasz Chruściel <lchrusciel@gmail.com>
2018-06-07 09:34:49 +0200

    [Maintenance] Remove SyliusShopApi plugin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,8 +27,6 @@ class AppKernel extends Kernel
             new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
             new \Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle(),
 
-            new \Sylius\ShopApiPlugin\ShopApiPlugin(),
-            new \League\Tactician\Bundle\TacticianBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
 
             new \AppBundle\AppBundle(),
