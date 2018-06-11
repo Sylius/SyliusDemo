c4c808e2608c5430099591bfe13a70bc1f03eea1
Łukasz Chruściel <lchrusciel@gmail.com>
2018-06-11 14:23:20 +0200

    [Composer] Remove rest of obsolete bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,14 +27,8 @@ class AppKernel extends Kernel
             new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
             new \Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle(),
 
-            new Nelmio\CorsBundle\NelmioCorsBundle(),
-
             new \AppBundle\AppBundle(),
         ];
-
-        if ('prod' === $this->getEnvironment()) {
-            $bundles[] = new \Snc\RedisBundle\SncRedisBundle();
-        }
 
         return array_merge(parent::registerBundles(), $bundles);
     }
