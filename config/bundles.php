1feb8cad2c864a71a35a9073c71d571eb1d07c45
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-10-01 10:23:07 +0200

    Migrations + Plugins configuration

--- config/bundles.php
+++ config/bundles.php
@@ -59,4 +59,12 @@ return [
     Sonata\DatagridBundle\SonataDatagridBundle::class => ['all' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
+    Sylius\CustomerReorderPlugin\SyliusCustomerReorderPlugin::class => ['all' => true],
+    Sylius\CustomerOrderCancellationPlugin\SyliusCustomerOrderCancellationPlugin::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Prooph\Bundle\ServiceBus\ProophServiceBusBundle::class => ['all' => true],
+    Sylius\InvoicingPlugin\SyliusInvoicingPlugin::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
+    Sylius\AdminOrderCreationPlugin\SyliusAdminOrderCreationPlugin::class => ['all' => true],
+    Sylius\RefundPlugin\SyliusRefundPlugin::class => ['all' => true],
 ];
