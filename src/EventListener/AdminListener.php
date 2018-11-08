d1bdfb294f894d4d37acc7f6155c297a4d4d1aed
Kamil Kokot <kamil@kokot.me>
2018-11-08 14:00:38 +0100

    Symfony 4 directory structure

---
+++ src/EventListener/AdminListener.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\EventListener;
+
+use Sylius\Component\Core\Model\AdminUserInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\File\Exception\UnexpectedTypeException;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+final class AdminListener
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @param Session $session
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param GenericEvent $event
+     */
+    public function preDelete(GenericEvent $event): void
+    {
+        $subject = $subject = $event->getSubject();
+
+        if (!$subject instanceof AdminUserInterface) {
+            throw new UnexpectedTypeException($subject, AdminUserInterface::class);
+        }
+
+        if ('sylius@example.com' === $subject->getEmailCanonical()) {
+            $this->session->getFlashBag()->add('error', 'sylius_demo.admin_account.prevent_delete');
+            $event->stopPropagation();
+        }
+    }
+
+    /**
+     * @param GenericEvent $event
+     */
+    public function preUpdate(GenericEvent $event): void
+    {
+        $subject = $subject = $event->getSubject();
+
+        if (!$subject instanceof AdminUserInterface) {
+            throw new UnexpectedTypeException($subject, AdminUserInterface::class);
+        }
+
+        if ('sylius@example.com' === $subject->getEmailCanonical()) {
+            $this->session->getFlashBag()->add('error', 'sylius_demo.admin_account.prevent_edit');
+            $event->stopPropagation();
+        }
+    }
+}
---
+++ src/EventListener/ChannelListener.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+final class ChannelListener
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    /**
+     * @param Session $session
+     */
+    public function __construct(Session $session, Router $router)
+    {
+        $this->session = $session;
+        $this->router = $router;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function preCreate(GetResponseEvent $event): void
+    {
+        if ('sylius_admin_channel_create' === $event->getRequest()->get('_route')) {
+            $this->session->getFlashBag()->add('error', 'sylius_demo.channel.prevent_create');
+
+            $url = $this->router->generate('sylius_admin_channel_index');
+            $response = new RedirectResponse($url);
+
+            $event->setResponse($response);
+        }
+    }
+}
