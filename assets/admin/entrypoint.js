aecaceaad0b6219d41e495637acdd3ac57874abc
Michał Pysiak <mpysiak@gmail.com>
2025-06-03 11:54:26 +0200

    Add ProductBundlePlugin

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -2,3 +2,4 @@ import '@vendor/sylius/refund-plugin/assets/entrypoint';
 import '@vendor/sylius/cms-plugin/assets/admin/entrypoint';
 import '@vendor/sylius/mollie-plugin/assets/admin/entrypoint';
 import '@vendor/sylius/wishlist-plugin/assets/admin/entrypoint';
+import '@vendor/sylius/product-bundle-plugin/assets/admin/entrypoint';
--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -13,14 +13,17 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
+use Sylius\ProductBundlePlugin\Entity\ProductBundlesAwareTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\MolliePlugin\Entity\ProductInterface;
 use Sylius\MolliePlugin\Entity\ProductTrait;
+use Sylius\ProductBundlePlugin\Entity\ProductInterface as ProductBundleAwareInterface;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_product')]
-class Product extends BaseProduct implements ProductInterface
+class Product extends BaseProduct implements ProductInterface, ProductBundleAwareInterface
 {
+    use ProductBundlesAwareTrait;
     use ProductTrait;
 }
