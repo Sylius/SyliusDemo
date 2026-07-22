d154e8137329ff0711007abb20d6094e44692f1d
Sajgon27 <szymon.mudrak@gmail.com>
2026-07-22 13:43:48 +0200

    Fix JS file

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,7 +1,6 @@
 import '@vendor/sylius/mollie-plugin/assets/shop/entrypoint';
 import '@vendor/flux-se/sylius-stripe-plugin/assets/shop/entrypoint';
+import '@vendor/sylius/adyen-plugin/assets/shop/entrypoint';
 
 import '../scripts/info_box'
 import './styles/main.scss'
-
-import '../../vendor/sylius/adyen-plugin/assets/shop/entrypoint';
